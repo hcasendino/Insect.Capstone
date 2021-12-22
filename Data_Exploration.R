eb8b9c8f183f05033f38f9becf3a103163b18284
hcasendino <hcas1024@uw.edu>
2021-12-21 17:36:52 -0800

    Cleaning

--- Data_Exploration.R
+++ Data_Exploration.R
@@ -1,18 +1,45 @@
-###===Insect Identification======
-# 7 November 2021
+###===Insect Characterisation of Bellingham Creeks======
+# Written by Helen Casendino (hcas1024@uw.edu)
+# Created: 7 Nov 2021   Modified: 21 Dec 2021
 ###=====================
 
 ###====Dependencies====
 library(here)
 library(tidyverse)
-###=====================
 
-#####====Read in Data - Ezza&Eily Data============
+
+#####====Read in Data - From Ezza & Eily ============
 COI_asvs <- read.csv("20211120.combined.COI.ASV.table.csv")
 MiFish_asvs  <- read.csv("20211120.combined.MiFish.ASV.table.csv")
 all_seq <- read.csv("master_sequencing_datasheet_20211026.csv")
+COI.all.previous.hashes.annotated <- readRDS("~/Desktop/Insect.Capstone/rds_files/COI.all.previous.hashes.annotated.rds")
 
-MiFish.all.previous.hashes.annotated 
+
+#####==============
+
+### COI ASV TABLE : Cleanup
+COI_asvs_ID_cols <- COI_asvs %>% 
+  separate(Sample_name, into = c("Locus", "MonthDay","Site","Reach","Bottle"), "[.]")
+
+# For rows with missing values, which locus? (mostly delta & MBT, and some COI)
+COI_asvs_ID_cols %>% 
+  filter_all(any_vars(is.na(.))) %>% 
+  group_by(Locus) %>% 
+  summarise(Count = n())
+
+# For the COI, which had missing values? (COI ones are all Kangaroo control) 
+COI_asvs_ID_cols %>% 
+  filter_all(any_vars(is.na(.))) %>% 
+  filter(Locus == "COI")
+
+COI_asvs_ID_cols %>% 
+  mutate(index = (1:nrow(COI_asvs_ID_cols))) %>% 
+  filter_all(any_vars(is.na(.)))
+
+
+
+
+COI.all.previous.hashes.annotated
 
 # COI: Which insects? 
 COI_annotated<- COI.all.previous.hashes.annotated
@@ -23,36 +50,3 @@ COI_annotated %>% filter(order == "Trichoptera") %>% group_by(family) %>% summar
 
 
 
-# =======WWU/City of Bellingham 2001-2003==========
-# From Joan (co-author of City of Bellingham Urban Streams Final Report 2006)
-
-# september 2002, 2001, 2003 (multiple sampling days/yr) at "Padden", "Squalicum", "Whatcom"...different sites at each watershed
-# each species has column, also rep column includes both numbers and letter designations? 
-insect_table <- read.csv(here("input/Bellingham_Urban_Streams_2006.csv"))
-
-# read in species code key 
-insect_key <- read.csv(here("input/Bellingham_sp_key.csv")) 
-sp_key <- insect_key[-c(1:8, 156:223),]
-colnames(sp_key) <- c("speciesCode", "order", "family", "genus")
-
-# I want to get a sense of most abundant benthic inverts for each watershed, across year/day/site/replicate. Can do deeper diver later
-
-invert_list <- list("Bellingham Watersheds - Benthic Inverts") # will hold ranked species lists
-watersheds <- c("Padden", "Squalicum", "Whatcom")
-
-for(i in 1:length(watersheds)){
-  
-  Grouped <- insect_table %>% 
-    filter(watershed == watersheds[i]) %>% 
-    select(-c(month,watershed, day, year, site, rep)) %>% 
-    pivot_longer(cols = -page, names_to = "speciesCode", values_to = "number") %>% select(-page)
-  
-  species_rank <- Grouped %>% 
-    group_by(speciesCode) %>% summarise(average = mean(number)) %>% 
-    arrange(desc(average)) # in order of desceding count for each species 
-  
-  annotated <- left_join(species_rank, sp_key, by = "speciesCode")
-  sp_rank_anno <- annotated %>% select(-speciesCode) %>% filter(average > 0)
-  
-  invert_list[[i+1]] <- sp_rank_anno
-}
