15b7ab42b775b697a4b651c52b0aaa7841981732
hcasendino <hcas1024@uw.edu>
2021-12-22 23:56:17 -0800

    Added argument for reach

--- eDNA_index_simple.r
+++ eDNA_index_simple.r
@@ -1,19 +1,22 @@
-# Usage eDNAindex(x); x is dataframe with columns: Sample, Hash, nReads, Biological.replicate
+# Usage eDNAindex(x); x is dataframe with columns: Sample, Hash, nReads, Biological.replicate (and Reach)
 # Co-opted from Moncho's code, but simplified syntax (only takes df, not each column as a string)
 
-eDNAindex<- function(df){ 
+eDNAindex<- function(df, reach = F){ 
 
   require(tidyverse)
-
+  
   df %>% 
-      group_by(Sample, Hash, Biological.replicate) %>%
+      group_by(Sample, Hash, Reach, Biological.replicate) %>%
       summarise (sumreads = sum(nReads)) %>%  # This sums technical replicates
       group_by(Sample,Biological.replicate) %>% 
       mutate (Tot = sum(sumreads),
               Row.prop = sumreads / Tot)  %>%         # This creates the proportion on each biological replicate    
       group_by(Sample) %>% 
       mutate (nreps = length(unique(Biological.replicate))) %>% 
-      group_by(Sample, Hash) %>% 
+      
+    {if (reach == TRUE){group_by(., Sample, Reach, Hash)}  
+      else{group_by(., Sample, Hash)} }  %>% 
+    
       summarise (mean.prop = sum (Row.prop) / max(nreps))   %>% #"Averaging ratios between Biological replicates"
       group_by (Hash) %>%
       mutate (Colmax = max (mean.prop),
