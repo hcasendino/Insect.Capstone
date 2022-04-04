7c4fc86c48f5fb07d7166bf3b70c65786b1c03bf
hcasendino <hcas1024@uw.edu>
2022-04-04 11:48:26 -0700

    combined two data wrangling scripts into this one

--- data wrangling/Insect_Taxonomy.R
+++ data wrangling/Insect_Taxonomy.R
@@ -1,16 +1,40 @@
-###===Insect Taxonomy of Bellingham Creeks - Indexed & Non-Indexed======
+###===Insect Taxonomy of Bellingham Creeks - ASV reads======
 # Written by Helen Casendino (hcas1024@uw.edu)
-# Created: 23 Dec 2021   Modified: 30 Jan 2022
+# Created: 23 Dec 2021   Modified: 4 Apr 2022
 ###=====================
 
 ###====Dependencies====
 library(tidyverse)
 library(here)
 
-#####====Read in Data ==========
+#####==== Read in Data & Clean ==========
+
 COI.hash.annotated <- readRDS(here("Input/COI.all.previous.hashes.annotated.rds"))
-COI_asv_reads_df <- read_csv(here("Input/COI_asv_reads_df.csv"))
-COI_index_output_df <- read_csv(here("Input/COI_index_output_df.csv"))
+COI_asvs <- read.csv(here("Input/20211230.combined.COI.ASV.table.csv"))
+
+### COI ASV TABLE : Cleanup
+COI_asvs_ID_cols <- COI_asvs %>% 
+  separate(Sample_name, into = c("Locus", "mmyy","Site","Reach","Biological.replicate"), "[.]")
+
+# For rows with missing values, which locus? (mostly delta & MBT, and some COI)(COI ones are all Kangaroo control) 
+COI_asvs_ID_cols %>% 
+  filter_all(any_vars(is.na(.))) %>% 
+  group_by(Locus) %>% 
+  summarise(Count = n())
+
+# Remove the rows with missing values
+rows_to_remove_df <- COI_asvs_ID_cols %>% 
+  mutate(index = (1:nrow(COI_asvs_ID_cols))) %>% 
+  filter_all(any_vars(is.na(.)))
+
+clean_COI_asvs <- COI_asvs_ID_cols %>% 
+  slice(-rows_to_remove_df$index) %>%  # remove rows
+  select(-Locus) %>% unite("Sample", c(mmyy, Site), sep = "_", remove = F) %>% 
+  filter(Site != "Kangaroo") # remove kangaroo
+
+### COI ASV TABLE : eDNA Reads (just renaming)
+
+COI_asv_reads_df <- clean_COI_asvs
 
 #####==== Adding Taxonomy ==========
 
@@ -18,16 +42,3 @@ COI.hash.annotated <- COI.hash.annotated %>% rename("Hash" = "representative")
 asv_reads_annotated <- left_join(COI_asv_reads_df, COI.hash.annotated, by = "Hash")
 write_csv(asv_reads_annotated, "Input/COI_reads_taxonomy.csv") 
 
-#####===== Adding Taxonomy to eDNA Indexed Data (March-April only) =========
-## ASV Table: aggregate for march/april (average index across mo)
-
-COI_index_MarAprOnly_df <- COI_index_output_df[-grep("^0821", COI_index_output_df$Sample, value = FALSE),] 
-118388
-COI_index_spring <- COI_index_MarAprOnly_df %>% 
-  select(Site, Hash, Normalized.reads) %>% 
-  distinct()  %>% # remove duplicate rows 
-  group_by(Site, Hash) %>% summarise(mean.Normalized.reads = mean(Normalized.reads))
-
-asv_index_annotated <- left_join(COI_index_spring, COI.hash.annotated, by = "Hash")
-
-write_csv(asv_index_annotated, "Input/COI_MarApr_index_taxonomy.csv") 
